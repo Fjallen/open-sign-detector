084abd767c9213928bb9ac2af1d6f1db3ff2c937
Fjallen <38442956+Fjallen@users.noreply.github.com>
2019-12-02 21:56:20 -0500

    Added model analysis and CNN works with exports

--- flip180.py
+++ flip180.py
@@ -2,8 +2,8 @@ import cv2
 
 
 for i in range(0,819):
-    name = "frame" + str(i) + ".jpg"
+    name = "up" + str(i) + ".jpg"
     image = cv2.imread(name)
     rotated = cv2.flip(image,0)
-    name = "uparrow" + str(i) + ".jpg"
+    name = "upSign" + str(i) + ".jpg"
     cv2.imwrite(name, rotated)
